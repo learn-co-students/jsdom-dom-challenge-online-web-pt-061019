835589c3cef0f0b628ab71a383e3eeaa9174cc1a
Brittany Javalera <brittjavalera@gmail.com>
2020-01-12 21:42:21 -0800

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       const userInputField = event.target.querySelector('#comment-input')
       const userInputString = userInputField.value
-      const newComment = document.createElement('li')
+      const newComment = document.createElement('p')
 
       newComment.textContent = userInputString
       commentsList.appendChild(newComment)
@@ -32,13 +32,52 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const pauseButton = document.getElementById("pause")
     pauseButton.addEventListener('click', function(){
-        clearInterval(timer);
-        minusButton.disabled = true;
-        addButton.disabled = true;
-        this.innerText = "resume";
+        if (pauseButton.innerText === "pause"){
+            clearInterval(timer);
+            minusButton.disabled = true;
+            addButton.disabled = true;
+            likeButton.disabled = true;
+            this.innerText = "resume";
+        }
+        else {
+            document.getElementById('minus').disabled = false;
+            document.getElementById('plus').disabled = false;
+            document.getElementById('heart').disabled = false;
+            this.innerText = "pause"
+            timer = setInterval(function() {
+                counter.innerHTML++;
+            }, 1000);
+        }
     });
 
     const likeButton = document.getElementById("heart")
+    let likeCounter = 1
+    likeButton.addEventListener('click', function(){
+        const likeList = document.querySelector(".likes")
+        const newLike = document.createElement('li')
+        newLike.textContent = `${counter.innerHTML} has ${likeCounter++} likes`
+        likeList.appendChild(newLike);
 
+    })
 
   });
+
+  //let pauseButton = document.querySelector('#pause')
+    
+  //pauseButton.addEventListener('click', (e) => {
+    //currentCount = counterEl.textContent;
+
+    //if (pauseButton.innerText === "pause") {
+    //    clearInterval(startCounting);
+    //    document.getElementById('minus').disabled = true;
+      //  document.getElementById('plus').disabled = true;
+        //document.getElementById('heart').disabled = true;
+      //  pauseButton.textContent = "resume";
+    //} else {
+      //  document.getElementById('minus').disabled = false;
+      //  document.getElementById('plus').disabled = false;
+     //   document.getElementById('heart').disabled = false;
+     //   pauseButton.textContent = "pause";
+     //   counter(); // or sertInetrval fucntion here
+   // }
+//})
